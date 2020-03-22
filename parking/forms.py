1b4c42eaef808c12b557fd23d42384673d584ccd
phidato <rizahmed.in@gmail.com>
2020-03-22 18:38:12 +0530

    add classes in form widget

--- parking/forms.py
+++ parking/forms.py
@@ -7,7 +7,7 @@ class CarInfoForm(forms.ModelForm):
         fields = ['car_number', 'in_time', 'out_time', 'date']
         widgets = {
             'car_number': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
-            'in_time': forms.DateTimeInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'out_time': forms.DateTimeInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'in_time': forms.DateTimeInput(attrs={'class': 'form-control datetimepicker1'}),
+            'out_time': forms.DateTimeInput(attrs={'class': 'form-control datetimepicker1'}),
+            'date': forms.DateInput(attrs={'class': 'form-control'}),
         }
